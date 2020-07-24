efc3f9b4a038d5358d30bf75099d2b8a20e51a3d
Romain.leduc <romain.leduc@outlook.fr>
2020-07-24 13:09:39 +0200

    Add pwa

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { router } from "./router";
 import Vuex from "vuex";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
